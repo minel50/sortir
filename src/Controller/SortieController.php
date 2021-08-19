b49017d7881cc6def550604d76802044dfb71e48
Thomas Huaume <thuaume1@gmail.com>
2021-08-19 17:43:06 +0200

    L'admin peut supprimer une sortie

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -382,10 +382,6 @@ class SortieController extends AbstractController
         $etatAnnule = $etatRepository->findOneBy(array('id' => 6));
         $sortie =  $sortieRepository->findOneBy(array('id' => $id));
 
-        //$dateActuelle =  date('H:i:s \O\n d/m/Y') > ;
-        $dateActuelle = new \DateTime('now');
-        //$endDateTime = clone $sortie->getDateHeureDebut();
-        dd($dateActuelle);
         if($sortie->getEtat()->getId() == 1 or $sortie->getEtat()->getId() == 2){
             $sortie->setEtat($etatAnnule);
         }
