d20eaf9849fadd3d906c19a339609ba5f5264642
Thomas Huaume <thuaume1@gmail.com>
2021-08-19 11:41:59 +0200

    Suppression des utilisateurs par l'admin + fix bug de suppression de toutes les photos si l'utilisateur upload sa première photo

--- migrations/Version20210817063410.php
+++ migrations/Version20210817063410.php
@@ -20,12 +20,12 @@ final class Version20210817063410 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE participant DROP roles, CHANGE photo photo VARCHAR(150) DEFAULT NULL');
+//        $this->addSql('ALTER TABLE participant DROP roles, CHANGE photo photo VARCHAR(150) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE participant ADD roles LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_bin`, CHANGE photo photo VARCHAR(150) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`');
+//        $this->addSql('ALTER TABLE participant ADD roles LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_bin`, CHANGE photo photo VARCHAR(150) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`');
     }
 }
