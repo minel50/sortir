1d0401c80d23c59c1efa379462724cfc33e8a84a
Thomas Huaume <thuaume1@gmail.com>
2021-08-20 09:52:50 +0200

    Ajout des assert unique campus et ville

--- src/Entity/Campus.php
+++ src/Entity/Campus.php
@@ -6,10 +6,12 @@ use App\Repository\CampusRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=CampusRepository::class)
+ * @UniqueEntity(fields={"nom"}, message="Ce campus existe déjà")
  */
 class Campus
 {
--- src/Entity/Ville.php
+++ src/Entity/Ville.php
@@ -6,10 +6,12 @@ use App\Repository\VilleRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=VilleRepository::class)
+ * @UniqueEntity(fields={"nom", "codePostal"}, message="Cette ville/code postal existe déjà")
  */
 class Ville
 {
